adf081305b63ccba901e6c9aaa22be89024caef6
krishnavamshich <42586313+krishnavamshich@users.noreply.github.com>
2018-08-22 00:46:32 +0530

    Update Stock_Buy_Sell.py

--- Stock_Buy_Sell.py
+++ Stock_Buy_Sell.py
@@ -1,15 +1,27 @@
 def maxProfit(arr, N):
     i = 0;
-    while(i<N):
+    sol = [];
+    while(i<N-1):
         #Go till you find a minimum price to buy
         while(i<N-1 and arr[i]>arr[i+1]):
             i+=1;
-        print("Buy :"+str(i), end="  ");
+        if(i==N-1):
+            break;
+        
+        x=i;
         #Go till you find maximum to sell
-        while(i<N-1 and arr[i]<arr[i+1]):
+        while(i<N-1 and arr[i]<=arr[i+1]):
             i+=1;
-        print("Sell :"+str(i));
-        i+=1;
+        y=i;
+        sol.append((x,y));
+    return sol;
+
 
 arr = [100, 180, 260, 310, 40, 535, 695];
-maxProfit(arr, len(arr));
+ans = maxProfit(arr, len(arr));
+
+for el in ans:
+    print("("+str(el[0])+" "+str(el[1])+")", end=" ");
+if(len(ans)==0):
+    print("No Profit", end="");
+print()
